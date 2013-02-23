aaadedc93774704045de1bc46f1989c3398880e5
haokaibo@gmail.com <haokaibo@gmail.com@f2942d09-3f10-7c9b-9b1d-0ad0b2ef6087>
2013-02-23 11:11:38 +0000

    1. Added the highCharts and jquery framework. 2. Drew two diagrams to show the Gold price trend charts.

---
+++ prototype/scripts/highcharts/goldPriceChart.js
@@ -0,0 +1,45 @@
+/* Gold price chart */
+$(document).ready(function() { 
+       var chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'container',
+                type: 'line'
+            },
+            title: {
+                text: 'Spot and Future Gold Price Charts'
+            },
+            subtitle: {
+                text: 'Source: WorldClimate.com'
+            },
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            yAxis: {
+                title: {
+                    text: 'Gold Price($)'
+                }
+            },
+            tooltip: {
+                enabled: true,
+                formatter: function() {
+                    return '<b>'+ this.series.name +'</b><br/>'+
+                        this.x +': '+ this.y +'$';
+                }
+            },
+            plotOptions: {
+                line: {
+                    dataLabels: {
+                        enabled: true
+                    },
+                    enableMouseTracking: true
+                }
+            },
+            series: [{
+                name: 'Spot',
+                data: [1507.0, 1506.9, 1509.5, 1510.5, 1518.4, 1521.5, 1525.2, 1526.5, 1523.3, 1518.3, 1513.9, 1509.6]
+            }, {
+                name: 'Future',
+                data: [1503.9, 1504.2, 1505.7, 1508.5, 1511.9, 1515.2, 1517.0, 1516.6, 1514.2, 1510.3, 1506.6, 1504.8]
+            }]
+        });
+});
---
+++ prototype/scripts/highcharts/highChartsGlobalSetting.js
@@ -0,0 +1,18 @@
+// Highcharts global settings
+$(function ($) {
+    Highcharts.setOptions({
+        chart: {
+            backgroundColor: {
+                linearGradient: [0, 0, 500, 500],
+                stops: [
+                    [0, 'rgb(255, 255, 255)'],
+                    [1, 'rgb(240, 240, 255)']
+                    ]
+            },
+            borderWidth: 2,
+            plotBackgroundColor: 'rgba(255, 255, 255, .9)',
+            plotShadow: true,
+            plotBorderWidth: 1
+        }
+    });
+});
